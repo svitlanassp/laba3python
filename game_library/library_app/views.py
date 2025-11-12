1b34fb71980b50ddfd64b441363ef09b76dbcd5a
Svitlana Potapova <svetas.potapova@gmail.com>
2025-11-12 22:57:42 +0200

    added viewsets

--- game_library/library_app/views.py
+++ game_library/library_app/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .serializers import UserSerializer, LibrarySerializer, OrderSerializer, LibraryGameSerializer, OrderGameSerializer
+from library_app.repositories.repository_manager import RepositoryManager
 
-# Create your views here.
+repo_manager = RepositoryManager()
+
+class BaseViewSet(viewsets.ViewSet):
+    repo = None
+    serializer_class = None
+
+    def list(self, request):
+        items = self.repo.get_all()
+        serializer = self.serializer_class(items, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        item = self.repo.get_by_id(pk)
+        if not item:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = self.serializer_class(item)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            obj = self.repo.create(**serializer.validated_data)
+            return Response(self.serializer_class(obj).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        item = self.repo.get_by_id(pk)
+        if not item:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(item, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            obj = self.repo.update(pk, **serializer.validated_data)
+            return Response(self.serializer_class(obj).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        deleted = self.repo.delete(pk)
+        if deleted:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class UserViewSet(BaseViewSet):
+    repo = repo_manager.users
+    serializer_class = UserSerializer
+
+class OrderViewSet(BaseViewSet):
+    repo = repo_manager.orders
+    serializer_class = OrderSerializer
+
+    @action(detail=False, methods=['get'])
+    def spending_report(self,request):
+        report_data = self.repo.get_user_spending_report()
+        return Response(list(report_data))
+
+
+class LibraryViewSet(BaseViewSet):
+    repo = repo_manager.libraries
+    serializer_class = LibrarySerializer
+
+class LibraryGameViewSet(BaseViewSet):
+    repo = repo_manager.library_games
+    serializer_class = LibraryGameSerializer
+
+    @action(detail=False, methods=['get'])
+    def popularity_report(self, request):
+        report_data = self.repo.get_game_popularity_report()
+        return Response(list(report_data))
+
+class OrderGameViewSet(BaseViewSet):
+    repo = repo_manager.order_games
+    serializer_class = OrderGameSerializer
+
