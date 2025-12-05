10ade5055a5f3b0fd447f8f1475dc42d8d332af8
Svitlana Potapova <svetas.potapova@gmail.com>
2025-12-05 13:09:07 +0200

    Delete game_library directory

--- game_library/library_app/repositories/game_genre_repository.py
+++
@@ -1,8 +0,0 @@
-from library_app.models import GameGenre
-from library_app.repositories.base_repository import BaseRepository
-
-
-class GameGenreRepository(BaseRepository):
-    def __init__(self):
-        super().__init__(GameGenre)
-
--- game_library/library_app/repositories/publisher_repository.py
+++
@@ -1,6 +0,0 @@
-from library_app.repositories.base_repository import BaseRepository
-from library_app.models import Publisher
-
-class PublisherRepository(BaseRepository):
-    def __init__(self):
-        super().__init__(Publisher)
