52b520c9fbb379afc5665b05085c2015d4ab1eb3
Svitlana Potapova <svetas.potapova@gmail.com>
2025-12-12 01:19:37 +0200

    fixed urls in html

--- game_library_api/library_app/pagination.py
+++ game_library_api/library_app/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class StandardPagination(PageNumberPagination):
-    page_size = 50 
+    page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
@@ -12,6 +12,4 @@ class StandardPagination(PageNumberPagination):
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data
-
         })
-
